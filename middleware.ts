f1e8eb3c64a21cf1e7d39c8a1a64a3335e56f8f8
Dre <montenegrodiussantos1@gmail.com>
2024-09-11 19:54:31 +0800

    refactor: Modify clerk middleware protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/", "/ask-question(.*)"]);
-
-export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
-});
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default clerkMiddleware();
 
 export const config = {
     matcher: [
